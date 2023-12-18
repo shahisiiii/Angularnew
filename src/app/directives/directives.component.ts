eccac443001da647b635c8ce51d691f16e858d78
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-18 10:25:31 +0530

    ngFor

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -10,5 +10,12 @@ export class DirectivesComponent {
 
   s:any=true
 
+  arr:any[]=[
+    {id:100,name:"shahis",email:"shahis@gmail.com"},
+    {id:101,name:"sham",email:"sham@gmail.com"},
+    {id:102,name:"shamil",email:"shamil@gmail.com"},
+    {id:103,name:"shanu",email:"shanu@gmail.com"}
+]
+
 
 }
