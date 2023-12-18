9c965704901d082dcb321d774e3fc7857132789c
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-18 10:02:17 +0530

    *ngIf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { Day1Component } from './day1/day1.component';
 import { EventComponent } from './event/event.component';
 import { TwowayComponent } from './twoway/twoway.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 const routes: Routes = [
     {path:'d1',component:Day1Component},
     {path:'envent',component:EventComponent},
-    {path:'twoway',component:TwowayComponent}
+    {path:'twoway',component:TwowayComponent},
+    {path:'directives',component:DirectivesComponent}
 ];
 
 @NgModule({
