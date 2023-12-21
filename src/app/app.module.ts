3463a9e8abf4f4c769bc46d31509eaf4b6fe85ca
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-21 09:41:30 +0530

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Day1Component } from './day1/day1.component';
 import { EventComponent } from './event/event.component';
-import {FormsModule} from '@angular/forms';
 import { TwowayComponent } from './twoway/twoway.component';
 import { DirectivesComponent } from './directives/directives.component';
 import { AtributdirectivesComponent } from './atributdirectives/atributdirectives.component';
 import { PipeComponent } from './pipe/pipe.component';
-import { FormsComponent } from './forms/forms.component'
+import { FormsComponent } from './forms/forms.component';
+import {FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { FormsComponent } from './forms/forms.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     provideClientHydration()
