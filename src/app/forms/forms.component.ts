3be7451933ac405fbbeeb77168593f120a6cad5e
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-21 10:03:54 +0530

    reactive form get value

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -9,7 +10,25 @@ export class FormsComponent {
 
 getData(data:any):any{
   console.log(data)
-    
+
+}
+
+form2:FormGroup
+
+constructor(public formBuild:FormBuilder){
+  this.form2=formBuild.group(
+    {
+      uname:[''],
+      psw:['']
+
+    }
+  )
+
+}
+
+show():void{
+  console.log(this.form2.value);
+  
 }
 
 }
