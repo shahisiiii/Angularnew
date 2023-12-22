b87212bc88f7f398d59635a0f8335777ff3b73e3
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-22 10:27:19 +0530

    error occured

--- src/app/mediator.service.ts
+++ src/app/mediator.service.ts
@@ -7,11 +7,11 @@ export class MediatorService {
 
   constructor() {}
   obj1:any={}
-   getValue(data:any):void{
-    console.log(data)
-    this.obj1=data
-    console.log(this.obj1);
-    
-    
-   }
+  getValue(data:any):void{
+  console.log(data)
+  this.obj1=data
+  console.log(this.obj1);
+  
+  
+  }
 }
